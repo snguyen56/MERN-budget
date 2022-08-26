2bed4832924721efb28f75763cf6bb65c976a2bc
myin1 <matthewyin1@gmail.com>
2022-08-25 20:02:54 -0400

    added signup and login functionality to the backend

---
+++ backend/models/userModel.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  // Vaidation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid Email");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  // Generate the salt
+  const salt = await bcrypt.genSalt(10);
+  // Generate the hashed password
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // Validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect Email");
+  }
+
+  // Compare the regular password and the hashed password
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect Password");
+  }
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
