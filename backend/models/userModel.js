77860527afb6e93f2964d33797e0d98e23db0c95
myin1 <matthewyin1@gmail.com>
2022-09-14 21:06:01 -0400

    created login function

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -22,6 +22,8 @@ userSchema.statics.signup = async function (email, password) {
   if (!email || !password) {
     throw Error("All fields must be filled");
   }
+  if (!validator.isEmail(email)) {
+  }
 
   const exists = await this.findOne({ email });
 
@@ -36,6 +38,29 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  // Checks if the email is in use
+  if (!user) {
+    throw Error("Incorrect Email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect Password");
+  }
 
   return user;
 };
