fb9bb1758faba7c8cb5f5cd3feba9bb73865fc05
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2022-10-09 14:26:34 -0400

    allow backend to serve frontend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const monthRoutes = require("./routes/monthRoutes");
 const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 
 const port = process.env.PORT || 4000;
 
@@ -19,6 +20,22 @@ app.use("/api/expense", expenseRoutes);
 app.use("/api/month", monthRoutes);
 
 app.use("/api/user", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname),
+      "../",
+      "frontend",
+      "build",
+      "index.html"
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Not set to production"));
+}
 
 mongoose
   .connect(process.env.MONGO_URI)
